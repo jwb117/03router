840d5d30dc88f7a64a62fbebf2db79b815e7c22e
jwb117 <jwbjoo@naver.com>
2021-06-16 15:50:37 +0900

    d

--- src/components/footer.vue
+++ src/components/footer.vue
@@ -1,6 +1,8 @@
 <template>
   <footer class="footer">
-      <div>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, laborum.</div>
+      <div>
+        <h3>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi, laborum.</h3>
+      </div>
   </footer>
 </template>
 
